3ad5d594c05c50eb9cf5d2bfae0a2f8be382c7c4
Shivansh1498 <rawatshivansh@gmail.com>
2024-02-21 16:45:07 +0530

    Made Navbar Responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,9 +10,10 @@ import "./Navbar.scss";
 
 const Navbar: FC = () => {
   return (
-    <nav className="navbar-container px-3 flex justify-content-space-between align-items-center">
+    <nav className="navbar-container px-page-default flex justify-content-space-between align-items-center">
       <h1>
-        <img src={MobileIcon} alt="heart icon" /> Overlay Store
+        <img src={MobileIcon} alt="heart icon" />
+        <span className="navbar-container_title">Overlay Store</span>
       </h1>
       <div className="search-input-container">
         <img src={SearchIcon} alt="heart icon" />
@@ -22,7 +23,7 @@ const Navbar: FC = () => {
           placeholder="Search for mobile skins"
         />
       </div>
-      <div className="right-section flex align-items-center flex-gap-1">
+      <div className="right-section flex align-items-center">
         <img src={HeartIcon} alt="heart icon" />
         <img src={ShoppingCartIcon} alt="shopping cart icon" />
         <img src={AvatarIcon} alt="avatar icon" />
